e339a1b2ccf23d717dfdda06c92a2fb525e3a564
kassioba <kassioluna@gmail.com>
2023-04-10 00:55:32 -0300

    bonus finalizado

--- src/app.js
+++ src/app.js
@@ -9,8 +9,13 @@ app.use(cors())
 const usuarios = []
 const tweets = [];
 
+
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
+
+  if (!username || !avatar || (typeof username || typeof avatar) !== "string") {
+    return res.status(400).send("Todos os campos são obrigatórios!");
+  }
 
   const novoUsuario = {
     username,
@@ -22,43 +27,65 @@ app.post("/sign-up", (req, res) => {
 });
 
 app.post("/tweets", (req, res) => {
-  const { username, tweet } = req.body;
+  const { tweet } = req.body;
+  const { user } = req.headers;
   const usuariosCadastrados = [];
   let imgUsuario;
+
+  if (!user || !tweet || (typeof user || typeof tweet) !== "string") {
+    return res.status(400).send("Todos os campos são obrigatórios!");
+  }
 
   for (let i = 0; i < usuarios.length; i++) {
     usuariosCadastrados.push(usuarios[i].username);
 
-    if (usuarios[i].username === username) {
+    if (usuarios[i].username === user) {
       imgUsuario = usuarios[i].avatar;
     }
   }
 
-  if (!usuariosCadastrados.includes(username)) {
-    res.send("UNAUTHORIZED");
+  if (!usuariosCadastrados.includes(user)) {
+    res.sendStatus(401);
     return;
   }
 
   const novoTweet = {
     avatar: imgUsuario,
-    username,
+    username: user,
     tweet,
   };
 
   tweets.push(novoTweet);
-  res.send("OK");
+  res.status(201).send("OK");
 });
 
-app.get('/tweets', (req, res) => {
-    const ultimosTweets = []
+app.get("/tweets", (req, res) => {
+  const ultimosTweets = [];
+  const page = req.query.page;
 
-    for(let i = 0; i < 10; i++){
-        if(tweets[i]){
-        ultimosTweets.push(tweets[i])
+  if (!page) {
+    for (let i = 0; i < 10; i++) {
+      if (tweets[i]) {
+        ultimosTweets.push(tweets[i]);
+      }
     }
-}
+  } else if (page < 1) {
+    return res.status(400).send("Informe uma página válida!");
+  } else {
+    for (let i = (page - 1) * 10; i < page * 10; i++) {
+      if (tweets[i]) {
+        ultimosTweets.push(tweets[i]);
+      }
+    }
+  }
 
-    res.send(ultimosTweets)
-})
+  res.send(ultimosTweets.reverse());
+});
+
+app.get("/tweets/:username", (req, res) => {
+  const { username } = req.params;
+
+  res.send(tweets.filter((tweet) => tweet.username === username).reverse());
+});
 
 app.listen(5000)
